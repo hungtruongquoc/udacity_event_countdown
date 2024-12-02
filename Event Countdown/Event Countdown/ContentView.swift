ef9121ecad26fba03599a957a56565ea046511d4
Hung Truong <hungtruongquoc@gmail.com>
2024-12-02 07:03:04 -0600

    Added a toolbar with plus button

--- Event Countdown/Event Countdown/ContentView.swift
+++ Event Countdown/Event Countdown/ContentView.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingEventForm = false
+    
     var body: some View {
-        VStack {
-            Text("Events")
-                .font(.title.bold())
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-            List {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+        NavigationStack {
+            VStack {
+                Text("Events")
+                    .font(.title.bold())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                List {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+                }
+            }
+            .background(Color(uiColor: .systemGroupedBackground))
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showingEventForm = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
             }
         }
-        .background(Color(uiColor: .systemGroupedBackground))
     }
 }
 
