513064c5f64395e8e48554170217d1e2a3abd87c
Hung Truong <hungtruongquoc@gmail.com>
2024-12-02 06:51:52 -0600

    Added label to the app

--- Event Countdown/Event Countdown/ContentView.swift
+++ Event Countdown/Event Countdown/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Events")
+                .font(.title.bold())
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            List {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+            }
         }
-        .padding()
     }
 }
 
